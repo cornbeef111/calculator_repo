0787d3070002e0c2af402284fe5ab5c3adf0b32b
cornbeef111 <edembaonyekachi@icloud.com>
2024-02-12 21:59:24 +0100

    done with calculator

--- scripts/calScript.js
+++ scripts/calScript.js
@@ -1,8 +1,68 @@
-import { clickDisplay } from "./number.js";
 
-let numberArray = [0,1,2,3,4,5,6,7,8,9];
-const buttons = document.querySelectorAll('.n'); /*getting all the button*/
+let firstNumber;
+let secondNumber;
+let step = 0;
+let operator;
+let result = 0;
 
-clickDisplay(buttons,numberArray);
+let numArray = [];
+let numArray2 = [];
+
+let display = document.querySelector('.forDisplay');
 
 
+function getNumber(num){
+    if(step === 0 || step === 1){
+        numArray.push(num);
+        step = 1;
+        firstNumber = Number(numArray.join(''));
+        display.innerHTML = firstNumber;
+    }else if(step === 2){
+        numArray2.push(num);
+        secondNumber = Number(numArray2.join(''));
+        display.innerHTML = secondNumber;
+    }
+}
+
+function getOperator(op){
+    step = 2;
+    operator = op;
+}
+
+function getDecimal(dec){
+    console.log(dec)
+}
+
+function clearDisplay(){
+    display.innerHTML = 0
+     firstNumber = null;
+     secondNumber = null;
+     step = 0;
+     operator = null;
+     result = 0;
+     numArray = [];
+     numArray2 = [];
+}
+
+function calculate(){
+    switch(operator){
+        case "+":
+          result = firstNumber + secondNumber
+          break;
+        case "-":
+          result = firstNumber - secondNumber
+          break;
+        case "*":
+          result = firstNumber * secondNumber
+          break;
+        case "/":
+          result = firstNumber / secondNumber
+          break;
+        case "%":
+          result = firstNumber % secondNumber
+          break;
+        default:
+          result = "choose a correct operator";
+    }
+    display.innerHTML = result;
+}
--- scripts/number.js
+++
@@ -1,9 +0,0 @@
-
-export function clickDisplay(buttons,array){
-    buttons.forEach(button =>{
-        button.addEventListener('click',() =>{
-            const index = parseInt(button.dataset.index);
-            const number = array[index];
-            return number;
-    })
-}
